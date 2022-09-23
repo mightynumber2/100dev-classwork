d0cad8a2b99c74b07fa2f7f97de0345ed9851b34
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-23 16:27:07 -0700

    Add 12 class-weekend-boring classwork

--- class-12-lab/class-weekend-boring/js/main.js
+++ class-12-lab/class-weekend-boring/js/main.js
@@ -5,6 +5,14 @@ function check() {
   const day = document.querySelector('#day').value
 
   //Conditionals go here
+  if (day === 'thursday' || day === 'tuesday') {
+    alert("Awesome day!");
+  } else if (day === 'saturday' || day === 'sunday') {
+    alert("Weekend");
+  } else {
+    alert("Boring day!");
+  }
+};
 
-
-}
+// Homework: remove case sensitivity for day input 
+// show result on the page
