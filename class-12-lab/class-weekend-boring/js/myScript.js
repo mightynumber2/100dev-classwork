91b3d7119f501f6c48bcc7bea138ac248e46c82c
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-24 09:54:50 -0700

    Create js file and complete class-weekend-boring homework

---
+++ class-12-lab/class-weekend-boring/js/myScript.js
@@ -0,0 +1,16 @@
+document.getElementById('check').onclick = checkDay;
+
+const showMessage = message => document.querySelector('#placeToSee').innerText = message; 
+
+function checkDay() {
+  const day = document.querySelector('#day').value;
+  if (day === 'tuesday' || day === 'thursday') {
+    showMessage('👊 GO GET!');
+  } else if (day === 'sunday') { 
+    showMessage('🎉 Weekend');
+  } else if (day === 'saturday') {
+    showMessage('✝ Sabbath... Rest');
+  } else {
+    showMessage('💤 Boooring...');
+  }
+};
