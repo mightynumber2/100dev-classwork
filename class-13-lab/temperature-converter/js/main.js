7406ce6cffee87abc2cde806734bd004eb23d47d
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-24 19:24:52 -0700

    Add 13 classwork

--- class-13-lab/temperature-converter/js/main.js
+++ class-13-lab/temperature-converter/js/main.js
@@ -1,1 +1,12 @@
 //Write your pseduo code first! 
+
+// You would need the temp in celsius to convert
+const tempCelsius = 50;
+
+// then you would need to convert to farenheit 
+function calcToFaren(temp) {
+  return (temp * 1.8) + 32;
+}
+
+// then show the calculation
+console.log(calcToFaren(tempCelsius));
