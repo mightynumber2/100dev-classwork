f1a998598ccfb5e203ce5063ad20fe1d6d24666d
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-27 11:26:11 -0700

    Complete 14 simple-functions classwork

--- class-14-lab/simple-functions/js/main.js
+++ class-14-lab/simple-functions/js/main.js
@@ -1,5 +1,15 @@
 //create a function that adds two numbers and alerts the sum
-
+function sumTwo(num1, num2) {
+  alert(num1 + num2);
+}
+sumTwo(5, 5);
 //create a function that multiplys three numbers and console logs the product
-
+function multThree(num1, num2, num3) {
+  console.log(num1 * num2 * num3);
+}
+multThree(2, 2, 3);
 //create a function that divides two numbers and returns the ???
+function divTwo(num1, num2) {
+  return num1 / num2;
+}
+console.log(divTwo(12, 4));
