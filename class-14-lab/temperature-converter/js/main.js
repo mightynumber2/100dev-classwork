09eee0a1298f1a24b5e649c809e4d71fa90e958e
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-09-27 10:04:39 -0700

    Complete 14 temp converter classwork

--- class-14-lab/temperature-converter/js/main.js
+++ class-14-lab/temperature-converter/js/main.js
@@ -1,1 +1,15 @@
 //Write your pseduo code first! 
+
+// on button click
+document.getElementById('check').onclick = convertToF;
+
+// convert temp from celsius to farenheit using a function
+function convertToF() {
+  // get temp in celsius
+  let tempInput = document.getElementById('temp').value;
+  tempInput = (tempInput * 9/5) + 32;
+  // show converted temp in dom
+  document.getElementById('placeToSee').textContent = tempInput + '*F';
+}
+
+
