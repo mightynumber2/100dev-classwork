554ae9b722e948cad2871dbd24acbffa7894481a
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-01 15:05:05 -0700

    Complete 19 arrays-fight-to-the-finish classwork

--- class-19-lab/arrays-fight-to-the-finish/js/main.js
+++ class-19-lab/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,30 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+const myMovies = ['The Mummy', 'Forrest Gump', 'Rocky'];
 
+myMovies.forEach((x, i) => document.querySelector('h2').textContent += x + " ");
+
+/* for (let i = 0; i < myMovies.length; i++) {
+  document.querySelector('h2').textContent += myMovies[i];
+} */
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
+const numArr = [7, 8, 9, 10];
 
+/* for (let i = 0; i < numArr.length; i++) {
+   numArr[i] += 3;
+}
+console.log(numArr); // 10, 11, 12, 13 */
+
+numArr.forEach((num, i) => numArr[i] = num + 3);
+console.log(numArr);
 
 //Find the average of all the numbers from question three
+let sum = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    sum += numArr[i];
+  }
+
+  // numArr.forEach((num, i) => sum += num);
+
+avgQ2 = sum / numArr.length;
+console.log(avgQ2);
