11c8d770f22f150bea306a21809ce11ea6d29283
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-05 19:13:27 -0700

    Complete 20 review-summer-games classwork

--- class-20-lab/review-summer-games/js/main.js
+++ class-20-lab/review-summer-games/js/main.js
@@ -1,1 +1,7 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiply(arr) {
+let product = 1;
+arr.forEach(num => product *= num)
+alert(product);
+}
+multiply([2, 2, 2, 2, 2]);
