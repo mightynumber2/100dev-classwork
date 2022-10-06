748548da18e58bdc41de6446358fe8c1cfc779ce
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-05 18:30:31 -0700

    Complete 20 review-in-paradise-after-paradise classwork

--- class-20-lab/review-in-paradise-after-paradise/js/main.js
+++ class-20-lab/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,13 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+const numbers = [7, 2, 3, 4, 5, 6];
+
+function compareArr(arr) {
+  if (arr[0] === arr[arr.length -1]) {
+    alert('We close in an hour');
+  } else if (arr[0] < arr[arr.length -1]){
+    alert('Hi');
+  } else {
+    alert('Bye');
+  }
+} 
+compareArr(numbers);
