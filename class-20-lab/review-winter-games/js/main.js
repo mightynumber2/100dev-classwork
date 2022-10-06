33124c5cdd5eb33f8f0b7e88c30a27dd77f322c8
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-05 20:03:57 -0700

    Complete 20 review-winter-games classwork

--- class-20-lab/review-winter-games/js/main.js
+++ class-20-lab/review-winter-games/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function getEvensFromArr(arr) {
+let evens = [];
+arr.forEach(x => {
+  if (x % 2 === 0) 
+    evens.push(x); 
+})
+return evens;
+}
+
+console.log(getEvensFromArr([1, 2, 3, 4, 5 ,6]));
