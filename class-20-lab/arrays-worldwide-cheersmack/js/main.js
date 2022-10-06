95a372cac21ab16483d1148e2480334748c01053
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-06 08:38:39 -0700

    Complete 20 arrays-worldwide-cheersmack homework

--- class-20-lab/arrays-worldwide-cheersmack/js/main.js
+++ class-20-lab/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+function arrayFromNum(num) {
+  let newArr = new Array();
+
+  for (let i = 1; i <= num; i++) {
+  newArr.push(i);
+  }
+return newArr;
+}
+console.log(arrayFromNum(10));
