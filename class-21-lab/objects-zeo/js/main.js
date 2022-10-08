f6a35b70a311a5b64a5f5071f4963638c85172d0
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-08 08:25:59 -0700

    Complete 21 objects-zeo classwork

--- class-21-lab/objects-zeo/js/main.js
+++ class-21-lab/objects-zeo/js/main.js
@@ -1,1 +1,11 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {
+  color: 'black',
+  shape: 'round',
+  minutes: 3,
+  seconds: 59,
+  startwatch: () => console.log('Start the timer'),
+  stopwatch: () => console.log('Stop the timer'),
+  mode: () => console.log('Set the watch time')
+}
+console.log(stopwatch.mode());
