22aca579430f977a72b9d2bdb8aa985bafb82ad8
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-08 20:47:00 -0700

    Update 21 #10 shuffle an array homework

--- class-21-lab/js-info-tasks/#10-shuffle-an-array/script.js
+++ class-21-lab/js-info-tasks/#10-shuffle-an-array/script.js
@@ -1,2 +1,13 @@
 ///////////////////////////////////////
 // SHUFFLE AN ARRAY
+
+let arr = [3, 2, 1];
+
+const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+console.log(shuffle(arr))
