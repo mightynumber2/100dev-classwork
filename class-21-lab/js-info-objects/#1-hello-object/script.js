3e11b2c41f633ed9e92b2a9e57fece69c956376f
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-12 10:05:05 -0700

    Complete js-info-objects #1 hello object

--- class-21-lab/js-info-objects/#1-hello-object/script.js
+++ class-21-lab/js-info-objects/#1-hello-object/script.js
@@ -1,6 +1,14 @@
 ///////////////////////////////////////
 // HELLO OBJECT
 
+const user = new Object();
+
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
+console.log(user);
 
 
 
