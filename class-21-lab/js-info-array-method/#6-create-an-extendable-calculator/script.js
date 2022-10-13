5c090eb1fb0091b5ae2f3e31c05d9c81bce3a1f5
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-13 15:39:31 -0700

    Practice 21 #6 create an extendable calculator

--- class-21-lab/js-info-array-method/#6-create-an-extendable-calculator/script.js
+++ class-21-lab/js-info-array-method/#6-create-an-extendable-calculator/script.js
@@ -1,2 +1,36 @@
 ///////////////////////////////////////
 // CREATE AN EXTENDABLE CALCULATOR
+
+function Calculator() {
+
+  this.methods = {
+    '+': (a, b) => a + b, 
+    '-': (a, b) => a - b, 
+  };
+
+  this.calculate = function(str) {
+    const split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2];
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b))
+    return NaN;
+    else return this.methods[op](a, b);
+  };
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func; 
+  }
+};
+// const cal = new Calculator();
+// console.log(cal.calculate('2 + 2'));
+
+let powerCalc = new Calculator;
+powerCalc.addMethod('*', (a,b) => a * b);
+powerCalc.addMethod('/', (a,b) => a / b);
+powerCalc.addMethod('**', (a,b) => a ** b);
+
+// console.log(powerCalc.methods);
+console.log(powerCalc.calculate('8 / 4'));
+
