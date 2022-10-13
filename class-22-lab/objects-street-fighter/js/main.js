82a362911d41d1e69644965b201bb36d4452080a
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-13 09:21:59 -0700

    Complete 22 objects-street-fighter classwork

--- class-22-lab/objects-street-fighter/js/main.js
+++ class-22-lab/objects-street-fighter/js/main.js
@@ -1,1 +1,23 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(gender, fightStyle, belt, country) {
+  this.gender = gender,
+  this.fightStyle = fightStyle,
+  this.belt = belt,
+  this.country = country,
+  this.attack = function() {
+    alert(`${this.fightstyle} attack!`)
+  }
+  this.dodge = function() {
+    alert('dodge!')
+  }
+  this.jumpAttack = function() {
+    alert(`jump ATTACK from ${this.country}!`)
+  }
+}
+
+const chunLi = new StreetFighter('female', 'gungFu', 'blue', 'China');
+
+StreetFighter.prototype.specialMove = true;
+
+console.log(chunLi.jumpAttack());
