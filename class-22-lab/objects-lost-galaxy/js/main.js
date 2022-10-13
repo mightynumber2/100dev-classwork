97aa1fa5645c6f08dd9786d2fd3c6a59925024e8
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-13 08:36:49 -0700

    Complete 22 objects-lost-galaxy classwork

--- class-22-lab/objects-lost-galaxy/js/main.js
+++ class-22-lab/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a mouse object that has four properties and three methods
+
+const mouse = {};
+
+mouse.make = 'Logitech';
+mouse.color = 'black';
+mouse.type = 'gaming';
+mouse.connection = 'wired';
+mouse.click = function () {
+  alert('CLICK!');
+}
+mouse.scroll = function () {
+  alert('scrolling...');
+}
+mouse.rightClick = function () {
+  alert('RIGHT click!');
+}
+
+console.log(mouse);
+
+
