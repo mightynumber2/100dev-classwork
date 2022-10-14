a3080674de31f12febdcb904d60c57fc27df61e7
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-14 13:11:38 -0700

    Complete 21 #6-create-an-extendable-calculator homework

--- class-21-lab/js-info-array-method/#6-create-an-extendable-calculator/script.js
+++ class-21-lab/js-info-array-method/#6-create-an-extendable-calculator/script.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////
 // CREATE AN EXTENDABLE CALCULATOR
 
-function Calculator() {
+/* function Calculator() {
 
   this.methods = {
     '+': (a, b) => a + b, 
@@ -32,5 +32,41 @@ powerCalc.addMethod('/', (a,b) => a / b);
 powerCalc.addMethod('**', (a,b) => a ** b);
 
 // console.log(powerCalc.methods);
-console.log(powerCalc.calculate('8 / 4'));
+console.log(powerCalc.calculate('8 / 4')); */
 
+// 2nd attempt
+function Calculator() {
+
+  this.methods = {
+    '+': (a, b) => a + b,  
+    '-': (a, b) => a - b,  
+  };
+
+  this.calculate = function(str) {
+    const split = str.split(" ");
+    a = +split[0];
+    op = split[1];
+    b = +split[2];
+    
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) 
+    return NaN 
+    else return this.methods[op](a, b); 
+  };
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+}
+
+const calc = new Calculator;
+console.log(calc.calculate('3 + 3'));
+
+const powerCalc = new Calculator;
+powerCalc.addMethod('*', (a, b) => a * b);
+powerCalc.addMethod('/', (a, b) => a / b);
+powerCalc.addMethod('**', (a, b) => a ** b);
+
+console.log(powerCalc.methods);
+console.log(powerCalc.calculate('2 * 4'));
+console.log(powerCalc.calculate('4 / 2'));
+console.log(powerCalc.calculate('4 ** 2'));
