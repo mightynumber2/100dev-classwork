83f470eff91d0a25248f8d12c2060838df9cdd7a
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-16 10:16:16 -0700

    Complete [23] space-review classwork

--- class-23-lab/space-review/js/main.js
+++ class-23-lab/space-review/js/main.js
@@ -1,12 +1,27 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let numbers = [2, 2, 2, 2,];
+alert( numbers.reduce((total, next) => total + next) );
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+function squareIt(arr) {
+  return arr.map(num => num ** 2);
+}
+console.log(squareIt(numbers));
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+function reverseStr(str) {
+  return str.split('').reverse().join('');
+}
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+function isPalindrome(str) {
+  console.log(reverseStr(str));
+  str === reverseStr(str) ? console.log(true) : console.log(false); 
+}
+
+isPalindrome('eye');
