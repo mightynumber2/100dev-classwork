c6b7f8b3c4ee4a74c65c7dd54f88b9296fcc0c8f
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-16 11:00:01 -0700

    Complete [23] objects-lost-galaxy classwork

--- class-23-lab/objects-lost-galaxy/js/main.js
+++ class-23-lab/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+
+const dog = {};
+
+dog.color = 'brown';
+dog.size = 'medium';
+dog.breed = 'old english';
+dog.tail = 'nub';
+dog.fart = function() {
+  console.log('BBBBBbbbbraaap!')
+};
+dog.sniff = function() {
+  console.log('SNIFF! sniff!')
+};
+dog.bark = function() {
+  console.log('Bbbbbbaaaaaaaarooooooooo!')
+};
+
+console.log(dog);
