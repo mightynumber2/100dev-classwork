3e996b5702c8b8d22db37fd167b5763278438e1c
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-16 11:14:03 -0700

    Complete [23] objects-tony-hawk-pro-skater classwork

--- class-23-lab/objects-tony-hawk-pro-skater/js/main.js
+++ class-23-lab/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,24 @@
-//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a Tony Hawk Pro Skater constructor that makes skating game characters with 4 properties and 3 methods
+
+function Skater(gender, boardBrand, size, skillLevel) {
+  this.gender = gender
+  this.boardBrand = boardBrand
+  this.size = size
+  this.skillLevel = skillLevel
+  this.flip = function() {
+    console.log('FLIP IT!');
+  }
+
+  this.ollie = function() {
+    console.log('Do an ollie!!!');
+  }
+
+  this.speedUp = function() {
+    console.log('Going faster!');
+  }
+}
+
+const tonyHawk = new Skater('male', 'Hawk\'s Brand', 'medium', 'master');
+
+console.log(tonyHawk);
+
