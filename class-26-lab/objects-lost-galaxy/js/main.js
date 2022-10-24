819a35ba161c83e6b15891872e1361d2a6cf997b
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-24 13:49:27 -0700

    Complete [26] objects-lost-galaxy classwork

--- class-26-lab/objects-lost-galaxy/js/main.js
+++ class-26-lab/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+const pizza = {};
+
+pizza.toppings = ['pepperoni', 'mushrooms'];
+pizza.crust = 'regular';
+pizza.cheese = true;
+pizza.size = 'extra-large';
+pizza.fold = function() {
+  console.log('FOLDING slice...');
+};
+pizza.microwave = function() {
+  console.log('Microwaving...');
+};
+pizza.toss = function() {
+  console.log('TOSSING pizza!');
+};
+
+console.log(pizza);
