708db3eb23f87e0a8d3ef0f73abbbf902fa5d704
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-24 14:05:14 -0700

    Complete [26] objects-constructor-fun classwork

--- class-26-lab/objects-constructor-fun/js/main.js
+++ class-26-lab/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+const Pizza = function(toppings, crust, cheese, size) {
+  this.toppings = toppings
+  this.crust = crust
+  this.cheese = cheese
+  this.size = size
+  this.fold = function() {
+    console.log('FOLDING slice...')
+  }
+  this.microwave = function() {
+    console.log('Microwaving...')
+  }
+  this.toss = function() {
+    console.log('TOSSING pizza!')
+  }
+  };
+  
+  const hawaiianPizza = new Pizza(['ham', 'pineapple'], 'deep-dish', true,'large');
+  
+  Pizza.prototype.sauce = 'spicy-red';
+
+  console.log(hawaiianPizza);
