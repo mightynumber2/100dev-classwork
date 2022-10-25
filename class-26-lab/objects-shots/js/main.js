7a84776f82313d0465043ea06c7f0b14a9e5e95f
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-10-24 17:12:28 -0700

    Complete [26] objects-shots classwork

--- class-26-lab/objects-shots/js/main.js
+++ class-26-lab/objects-shots/js/main.js
@@ -1,1 +1,31 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+/* fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.drinks)
+    document.querySelector('button').onclick = function() {
+        data.drinks.find(drink => { 
+          if (document.querySelector('input').value === drink.strDrink) {  
+          document.querySelector('h2').textContent = drink.strDrink;
+          document.querySelector('img').src = drink.strDrinkThumb;
+          document.querySelector('h3').textContent = drink.strInstructions;
+          }
+        })
+      }
+  })
+  .catch(err => console.log(`error ${err}`)); */
+  
+document.querySelector('button').onclick = function() {
+  const drink = document.querySelector('input').value;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.drinks);
+      document.querySelector('h2').textContent = data.drinks[0].strDrink;
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+      document.querySelector('h3').textContent = data.drinks[0].strInstructions;
+    })
+    .catch(err => console.log(`error: ${err}`));
+};
