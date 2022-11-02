deefa6451849e5c74a6f03d9034067ddb3de151b
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-02 14:38:15 -0700

    Complete [27] objects-netflix-tv-shows classwork

--- class-27-lab/objects-netflix-tv-shows/js/main.js
+++ class-27-lab/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,26 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetflixShow {
+  constructor(numOfChars, setting, genre, story) {
+  this.numOfChars = numOfChars
+  this.setting = setting
+  this.genre = genre
+  this.story = story
+  }
+  
+  play() {
+    console.log('Playing...')
+  }
+  stop() {
+    console.log('Stopping...')
+  }
+  showAddToWatchList() {
+    console.log('Added to watchlist')
+  }
+}
 
+const theExcorcists = new NetflixShow(4, 'haunted house', 'horror', 'exorcists vs haunted house');
+
+console.log(theExcorcists);
+
+NetflixShow.prototype.holyWater = true;
