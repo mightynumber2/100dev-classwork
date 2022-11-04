aacab01aba5a0c2fba29428b49598546c9d8d3fe
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-03 20:35:52 -0700

    Complete [27] local-storage-botScore-button homework

--- class-27-lab/local-storage-botScore-button/js/main.js
+++ class-27-lab/local-storage-botScore-button/js/main.js
@@ -1,3 +1,19 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
+let score = document.querySelector('h3')
 
+document.querySelector('#add').onclick = function() {
+  if (!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore', 0)
+  }
+
+  let localBotScore = +localStorage.getItem('botScore')
+  localBotScore += 1;
+  localStorage.setItem('botScore', localBotScore)
+  score.textContent = localBotScore;
+};
+
+document.querySelector('#reset').onclick = function() {
+  localStorage.clear();
+  score.textContent = 'Score';
+};
