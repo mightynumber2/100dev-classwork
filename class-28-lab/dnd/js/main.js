4efd12b4625e16d372acfeb1dae51588a5660bf5
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-08 18:15:13 -0700

    [28] Complete dnd classwork

--- class-28-lab/dnd/js/main.js
+++ class-28-lab/dnd/js/main.js
@@ -2,16 +2,32 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
+  const choice = document.querySelector('input').value.toLowerCase().split(' ').join('-')
   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data)
+        document.querySelector('h2').textContent = data.name;
+        
+        data.classes.forEach(obj => {
+            const classList = document.createElement('li')
+            
+            classList.textContent = obj.name
+            document.querySelector('#classes').appendChild(classList)
+        })
+
+        data.subclasses.forEach(obj => {
+            const subclassList = document.createElement('li')
+
+            subclassList.textContent = obj.name
+            document.querySelector('#subclasses').appendChild(subclassList)
+        }) 
+            
       })
       .catch(err => {
-          console.log(`error ${err}`)
+          console.log(`error - ${err}`)
       });
 }
 
