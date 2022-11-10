b5b70da4d8e8db444951a4c09581326fef704e32
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-09 23:05:06 -0700

    [28] Start open-library-api classwork using api-template

--- class-28-lab/api-template/js/main.js
+++ class-28-lab/api-template/js/main.js
@@ -4,21 +4,14 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+  const url = `https://openlibrary.org/isbn/${choice}.json`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
-        }
-       
-        document.querySelector('h3').innerText = data.explanation
+        document.querySelector('h2').textContent = data.title;
+        document.querySelector('h3').textContent = choice;
       })
       .catch(err => {
           console.log(`error ${err}`)
