e3b7d0b00ba01fa68d9a37473f9b515e79b21c14
DVo <83097985+mightynumber2@users.noreply.github.com>
2022-11-10 14:22:58 -0700

    [28] Complete open-library-api classwork using api-template

--- class-28-lab/api-template/js/main.js
+++ class-28-lab/api-template/js/main.js
@@ -1,5 +1,20 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+const bookUL = document.querySelector('ul')
+const bookList = document.createElement('li')
+const localBooks = JSON.parse(localStorage.getItem('books'))
+let myList = []
+// Display the book list from locale storage if any
+if (localBooks) {
+  myList = localBooks
+  localBooks.forEach(book => {
+  bookUL.innerHTML += book
+  bookUL.appendChild(document.createElement('hr'))
+ } )
+}
+console.log(myList)
+
+// Add book via ISBN
+document.querySelector('#isbn').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
@@ -10,11 +25,27 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('h2').textContent = data.title;
-        document.querySelector('h3').textContent = choice;
+        // Display book to list
+        const bookList = document.createElement('li')
+        document.querySelector('ul').appendChild(bookList)
+        bookList.textContent = data.title
+        // Add book to storage
+        myList.push(data.title)
+        localStorage.setItem('books', JSON.stringify(myList))
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+// 9780140328721
+// 0201558025
+// 0385472579
+
+// Clear local storage
+document.querySelector('#clear').onclick = function() {
+  localStorage.clear()
+  myList = []
+  bookUL.innerHTML = ''
 }
 
